9b1d897571b82c3e35ec38fe8e455529cb795f70
kdain0105 <kdain0105@gmail.com>
2026-03-03 18:19:40 +0900

    우왕

--- src/ch01_variable_operator/CondOper.java
+++ src/ch01_variable_operator/CondOper.java
@@ -4,7 +4,7 @@ public class CondOper {
     static void main() {
 
         int x =3, y = 8, z = 4;
-        int max = x > y ? x : y;
+        int max = x > y ? x : y; // int max = (x>y)?(x>z?x:z):(y>z?y:z);
         max = max > z ? max : z;
 
         System.out.println("출력 결과 : 3, 8, 4 중 최대 수는" + max + "입니다.");
