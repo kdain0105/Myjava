4452204ad63895ebf75de0009e3354df99b98f26
kdain0105 <kdain0105@gmail.com>
2026-03-03 15:15:33 +0900

    t ya

--- src/ch01_variable_operator/Casting.java
+++ src/ch01_variable_operator/Casting.java
@@ -13,9 +13,21 @@ public class Casting {
         //(double)14/5 => 명시적 14.0/5 => 암시적 14.0/5.0 => 2.8
         System.out.println((double)14/5);// (정수)/(정수) = (정수)
 
-
         // (double)(14/5) => (double)2 => 2.0
         System.out.println((double)(14/5));// (정수)/(정수) = (정수)
+
+        int kor = 50, eng = 60, math = 80 ;
+        int total = kor + eng + math;
+        System.out.println("총점 : " + total);
+
+        double average = (double)total/3 ;
+        System.out.println("평균 : " + average);
+
+        char ch1 ='c';
+        char ch2 ='a';
+        boolean bool1 = ch1 > ch2 ;
+        System.out.println("bool1 : " + bool1 );
+
     }
 }
 
